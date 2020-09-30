c0c1965c56e48bb9464eb6449f810869708d1f07
petros <pit.trak@gmail.com>
2020-09-30 13:04:57 +0300

    Added mongoose error handling (Validation)

--- middleware/error.js
+++ middleware/error.js
@@ -8,6 +8,19 @@ const errorHandler = (err, req, res, next) => {
     // Log to console for dev
     console.log(err.stack.red)
 
+    // Mongoose duplicate key
+    if(err.code === 11000) {
+        const message = 'Duplicate field value entered'
+        error = new ErrorResponse(message, 400)
+    }
+
+    // Mongoose validation error
+    if(err.name === 'ValidationError') {
+        // extract message value from an array of objects
+        const message = Object.values(err.errors).map(val => val.message)
+        error = new ErrorResponse(message, 400)
+    }
+
     // Mongoose bad ObjectId
     if(err.name === 'CastError') {
         const message = `Resource not found with id of ${err.value}`
