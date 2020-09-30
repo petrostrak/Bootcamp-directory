ac2e2adef978f38250eb068367ce1e8dd65d036c
petros <pit.trak@gmail.com>
2020-09-30 12:52:50 +0300

    Added mongoose error handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,7 +10,7 @@ exports.getBootcamps = async (req, res, next) => {
 
         res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
     } catch (err) {
-        res.status(400).json({ success: false })
+        next(err)
     }
 
 }
@@ -28,10 +28,8 @@ exports.getBootcamp = async (req, res, next) => {
 
         res.status(200).json({ success: true, data: bootcamp })
     } catch (err) {
-        next(new ErrorRespose(`Bootcamp not found with id of ${req.params.id}`, 404))
+        next(err)
     }
-
-    res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` })
 }
 
 // @desc    Create single bootcamp
@@ -47,7 +45,7 @@ exports.createBootcamp = async (req, res, next) => {
             data: bootcamp
         })
     } catch (err) {
-        res.status(400).json({ success: false })
+        next(err)
     }
 
 }
@@ -64,12 +62,12 @@ exports.updateBootcamp = async (req, res, next) => {
         })
 
         if(!bootcamp) {
-            return res.status(400).json({ success: false })
+            return next(new ErrorRespose(`Bootcamp not found with id of ${req.params.id}`, 404))
         }
 
         res.status(200).json({ success: true, data: bootcamp })
     } catch (err) {
-        res.status(400).json({ success: false })
+        next(err)
     }
 }
 
@@ -82,11 +80,11 @@ exports.deleteBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
 
         if(!bootcamp) {
-            return res.status(400).json({ success: false })
+            return next(new ErrorRespose(`Bootcamp not found with id of ${req.params.id}`, 404))
         }
 
         res.status(200).json({ success: true, data: {} })
     } catch (err) {
-        res.status(400).json({ success: false })
+        next(err)
     }
 }
