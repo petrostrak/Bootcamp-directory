29e00baf2d4233f453659bd409e6606f1c743914
petros <pit.trak@gmail.com>
2020-10-02 11:23:27 +0300

    Created login for user

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/User')
 const asyncHandler = require('../middleware/async')
 
 // @desc    Register User
-// @route   GET /api/v1/auth/register
+// @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
     const { name, email, password, role } = req.body
@@ -16,7 +16,41 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     })
 
-    // Create tokem
+    // Create token
+    const token = user.getSignedJwtToken()
+
+    res.status(200).json({
+        success: true,
+        token
+    })
+})
+
+// @desc    Login User
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body
+
+    // Validate email & password
+    if(!email || !password) {
+        return next(new ErrorRespose('Please provide an email and password', 400))
+    }
+
+    // Check for a user
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user) {
+        return next(new ErrorRespose('Please enter valid credentials', 401))
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password)
+
+    if(!isMatch) {
+        return next(new ErrorRespose('Please enter valid credentials', 401))
+    }
+
+    // Create token
     const token = user.getSignedJwtToken()
 
     res.status(200).json({
