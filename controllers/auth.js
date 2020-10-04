052c642584196a85075d0b0c75f78c35b59b5555
petros <pit.trak@gmail.com>
2020-10-04 12:07:45 +0300

    Added forgot password functionality b sending email back to user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorRespose = require('../utils/errorResponse')
 const User = require('../models/User')
+const sendEmail = require('../utils/sendEmail')
 const asyncHandler = require('../middleware/async')
 
 // @desc    Register User
@@ -73,6 +74,30 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     const resetToken = user.getResetPasswordToken()
 
     await user.save({ validateBeforeSave: false })
+
+    // Create reset url
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`
+
+    const message = `You are receiving this email because you (or someone else) has requested
+    the reset of a password. Please make PUT request to: \n\n ${resetUrl}`
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Password reset token',
+            message
+        })
+
+        res.status(200).json({ success: true, data: 'Email sent' })
+    } catch (err) {
+        console.log(err)
+        user.resetPasswordToken = undefined
+        user.resetPasswordExpire = undefined 
+
+        await user.save({ validateBeforeSave: false })
+
+        return next(new ErrorRespose('Email could not be sent', 500))
+    }
 
     res.status(200).json({
         success: true,
--- seeder.js
+++ seeder.js
@@ -1,15 +1,15 @@
-const fs = require('fs')
 const mongoose = require('mongoose')
-const colors = require('colors')
-const dotenv = require('dotenv')
+const colors   = require('colors')
+const dotenv   = require('dotenv')
+const fs       = require('fs')
 
 // Load env variables
 dotenv.config({ path: './config/config.env' })
 
 // Load models
 const Bootcamp = require('./models/Bootcamp')
-const Course = require('./models/Course')
-const User = require('./models/User')
+const Course   = require('./models/Course')
+const User     = require('./models/User')
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -21,8 +21,8 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
-const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
-const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'))
+const courses   = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
+const users     = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'))
 
 // Import into DB
 const importData = async () => {
